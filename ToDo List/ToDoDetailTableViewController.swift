5062c56e72f8412cf1b614fb4185fb981dcd7a7a
Alvin Chen <chenbog@bc.edu>
2020-09-26 17:43:29 -0400

    5.2,5.3 done

---
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDo List
+//
+//  Created by Alvin Chen on 9/26/20.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentinginAddMode = presentingViewController is UINavigationController
+        if isPresentinginAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
