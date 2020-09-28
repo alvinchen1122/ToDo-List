34ded5d4c0d49a66eb4aab4a78f6ae4e605cf0fe
Alvin Chen <chenbog@bc.edu>
2020-09-27 23:37:33 -0400

    5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Alvin Chen on 9/27/20.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
